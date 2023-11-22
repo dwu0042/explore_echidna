82f66d4ff5dacb0cb0e37e77f9ab6b89e91d999c
dwu0042 <david.wu1@monash.edu>
2023-11-22 18:04:15 +1100

    remove sankey from cluster

--- clusters.py
+++ clusters.py
@@ -68,57 +68,6 @@ def multi_infomap(G_base: str|Path, conv=True):
 
     return df
 
-def map_multidf_to_sankey(df: pl.DataFrame):
-    """ Brute-force map of a dataframe of clusterings of nodes to Sankey diagram
-    """
-
-    # compute n clusters, for sankey indexing
-    n_clusters = df.max().to_numpy()[:,1:].flatten().cumsum()
-
-    sankey_edges = pl.DataFrame(schema={
-        'source': pl.Int32, 
-        'target': pl.Int32, 
-        'value': pl.Int32,
-    })
-
-    sankey_nodes = pl.DataFrame(schema={
-        'node': pl.Int32,
-        'x': pl.Float64,
-        'y': pl.Float64,
-    })
-
-    # compute flows
-    for i, ((n0, col0), (n1, col1)) in enumerate(pairwise(zip(chain([0], n_clusters), df.columns[1:]))):
-        flows = (df.lazy().select(col0, col1)
-                 .group_by('*')
-                 .count()
-                 .drop_nulls()
-                 .select(
-                     (pl.col(col0) + n0).alias('source').cast(pl.Int32),
-                     (pl.col(col1) + n1).alias('target').cast(pl.Int32),
-                     pl.col('count').alias('value').cast(pl.Int32),
-                    )
-        ).collect()
-        sankey_edges = sankey_edges.vstack(flows)
-
-    for i, (col, (n0, n1)) in enumerate(zip(df.columns[1:], pairwise(chain([0], n_clusters)))):
-        nodes = (df.lazy()
-                .select(col)
-                .drop_nulls()
-                .unique()
-                .with_columns(
-                    x=(i)/(len(df.columns)-1),
-                    y=pl.col(col) / (n1 - n0),
-                )
-                .select(
-                    (pl.col(col) + n0).alias('node').cast(pl.Int32),
-                    pl.col('x').cast(pl.Float64),
-                    pl.col('y').cast(pl.Float64),
-                )).collect()
-        sankey_nodes = sankey_nodes.vstack(nodes)
-
-    return sankey_nodes, sankey_edges
-
 def heat_wavelet(G: ig.Graph, tau=1.2, normalised=True):
     assert not G.is_directed(), "Laplacian not defined for directed networks"
     A = np.array(G.get_adjacency().data)
