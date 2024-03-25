91f61e30ce2882a8220cdef8871d9a21f04b053b
dwu0042 <david.wu1@monash.edu>
2024-03-25 22:39:52 +1100

    Add a null iterable to send empty appends to

--- util.py
+++ util.py
@@ -7,3 +7,13 @@ class Iden():
     
     def __setitem__(self, x):
         raise TypeError("Identity objects do not support assignment")
+    
+class NullIterable():
+    def __init__(self):
+        pass
+
+    def append(self, item):
+        pass
+
+    def __iter__(self):
+        yield
