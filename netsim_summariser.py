11321edfe000f6b587385974a4df368fb1085512
dwu0042 <david.wu1@monash.edu>
2024-10-03 15:52:04 +1000

    Add extent metric computations

--- netsim_summariser.py
+++ netsim_summariser.py
@@ -3,6 +3,7 @@ import polars as pl
 import numpy as np
 import multiprocessing
 from itertools import repeat
+from functools import wraps
 
 class Summariser():
     """Utility class to sumamrise metrics from an h5 database of simulation outputs"""
@@ -11,26 +12,39 @@ class Summariser():
         self.target_file = file
 
     @staticmethod
+    def _sanitise(fn):
+        @wraps(fn)
+        def sanitised_fn(*args, **kwargs):
+            return list(fn(*args, **kwargs))
+        return sanitised_fn
+
+    @staticmethod
+    @_sanitise
     def hitting_time(target : h5py.Group, null=np.nan, **kwargs):
         """Yields list of hitting times to all locations"""
         mask = (target['history'][:] != 0)
-        return list(np.where(mask.any(axis=1), mask.argmax(axis=1), null))
+        indices = np.where(mask.any(axis=1), mask.argmax(axis=1), null)
+        times = np.nan * np.empty_like(indices)
+        times[np.isfinite(indices)] = target['ts'][:][indices[np.isfinite(indices)].astype(int)]
+        return times
+
+    # @staticmethod
+    # def extent(target: h5py.Group, time_limit=30, **kwargs):
+    #     """Yields the number of locations hit by the time limit"""
+    #     hitting_times = np.array(Summariser.hitting_time)
+    #     return (
+    #         sum(np.where(mask.any(axis=1), mask.argmax(axis=1), np.nan) < time_limit, -1),
+    #         time_limit,
+    #     )
 
     @staticmethod
-    def extent(target: h5py.Group, time_limit=30, **kwargs):
-        """Yields the number of locations hit by the time limit"""
-        mask = (target['history'][:] != 0)
-        return (
-            sum(np.where(mask.any(axis=1), mask.argmax(axis=1), np.nan) < time_limit, -1),
-            time_limit,
-        )
-
-    @staticmethod
+    @_sanitise
     def movement_out_total(target: h5py.Group, **kwargs):
         """Yields the list of number of movements out of all locations"""
         return list(target['mover_out'][:].sum(axis=1))
 
     @staticmethod
+    @_sanitise
     def movement_in_total(target: h5py.Group, **kwargs):
         """Yields the list of number of movements into all locations"""
         return list(target['mover_in'][:].sum(axis=1))
@@ -78,7 +92,7 @@ class Summariser():
                 self.hitting_time,
                 self.steady_state_infected,
                 self.emptying_time,
-                self.extent,
+                # self.extent,
             ]
             if not no_move:
                 metrics += [
@@ -138,14 +152,33 @@ class Summariser():
                 pl.col('steady_state_infected').list.to_struct(
                     fields=['ssi_mean', 'ssi_std']
                 ),
-                pl.col('extent').list.to_struct(
-                    fields=['extent', 'extent_time']
-                )
+                # pl.col('extent').list.to_struct(
+                #     fields=['extent', 'extent_time']
+                # )
             )
             .unnest(
                 'hitting_time',
                 'steady_state_infected',
-                'extent',
+                # 'extent',
             )
             .fill_nan(None)
+        )
+
+    def metrics(self, over=None, ncpus=4, no_move=False, verbose=False, drop=(), strict_drop=True, **kwargs):
+
+        results = self.collect(over=over, ncpus=ncpus, no_move=no_move, verbose=verbose, **kwargs)
+        df = self.results_to_polars(results, drop=drop, strict_drop=strict_drop)
+
+        return Metrics(df)
+
+
+class Metrics(pl.DataFrame):
+
+    def add_extent(self, extent_time=30):
+        return self.with_columns(
+            pl.concat_list(pl.selectors.starts_with('hitting_time'))
+            .list.eval(pl.element().lt(extent_time))
+            .list.sum().sub(1)
+            .alias('extent'),
+            pl.lit(extent_time).alias('extent_time')
         )
