d910160cd3eef4709eb9d476c1483e2e8916a63c
dwu0042 <david.wu1@monash.edu>
2024-10-10 16:33:27 +1100

    Add/edit zero sim run code

--- run_zero_temporal_sim.py
+++ run_zero_temporal_sim.py
@@ -6,12 +6,11 @@ import network_simulation as simu
 def preload():
 
     ordering = conv.Ordering.from_file("./concordant_networks/size_14.csv")
-    converter = conv.StaticConverter.from_file(
-        "./concordant_networks/static_base_1_threshold_4.graphml", 
-        ordering,
-        purge_selfloops=True,
+    converter = conv.TemporalNetworkConverter.from_file(
+        "concordant_networks/temponet_14_365.lgl", 
+        ordering=ordering,
+        weight='weight',
     )
-
 
     return ordering, converter
 
@@ -26,7 +25,15 @@ def gen_params(converter):
         'prob_final_stay': prob_final_zero,
     })
 
-def create_sim(converter, parameters, pseudo_capacity=False, seeds=0, numinfperseed=0, dt=1.0, track_movement=True):
+def create_sim(
+        converter: conv.TemporalNetworkConverter, 
+        parameters, 
+        pseudo_capacity=False,
+        seeds=0, 
+        numinfperseed=0, 
+        dt=1.0, 
+        track_movement=True,
+):
 
     if pseudo_capacity:
         # fake the size of the hospitals
@@ -35,10 +42,12 @@ def create_sim(converter, parameters, pseudo_capacity=False, seeds=0, numinfpers
         full_sizes = converter.ordering.sizes
 
 
-    sim = simu.StaticWithHome(
+    sim = simu.TemporalSim(
         full_size=full_sizes,
         parameters=parameters,
         dt=dt,
+        num_times=converter.NT,
+        discretisation_size=converter.DT,
         track_movement=track_movement,
     )
 
@@ -49,7 +58,7 @@ def create_sim(converter, parameters, pseudo_capacity=False, seeds=0, numinfpers
 
     return sim
 
-def simulate_sim_and_record(simulation: simu.SimulationWithMovers, simid, until=100, nostop=False, outfile=None, with_movers=False, **kwargs):
+def simulate_sim_and_record(simulation: simu.TemporalSim, simid, until=100, nostop=False, outfile=None, with_movers=False, **kwargs):
 
     simulation.simulate(until=until, nostop=nostop)
 
@@ -62,17 +71,12 @@ def run_sims(n_reps=20):
 
     ordering, converter = preload()
     parameters = gen_params(converter)
-    sim = create_sim(converter, parameters=parameters, pseudo_capacity=True)
+    sim = create_sim(converter, parameters=parameters, pseudo_capacity=True, dt=0.5)
 
-    # skips = set([i for i,row in enumerate(converter.outwards_weighting_matrix) if row.nnz == 0])
-    skips = {149}
 
-    for _ in range(n_reps):
+    for _n in range(n_reps):
         for i,v in enumerate(converter.ordering.sizes):
             print(i, v, sep=',', end=' ', flush=True)
-            if i in skips:
-                print('X', sep='', end='', flush=True)
-                continue
 
             sim.reset(soft=False)
             sim.state[i] = 30
@@ -80,8 +84,8 @@ def run_sims(n_reps=20):
             now = datetime.datetime.now()
             simdate = now.strftime('%y%m%d')
             simtime = now.strftime('%H%M%S')
-            simid = int(simdate + simtime)
-            outname = f"zero_sims/static/sim_all_30s.h5"
+            simid = (i, _n, simdate, simtime)
+            outname = f"zero_sims/temporal/sim_all_30s.h5"
             simulate_sim_and_record(sim, simid=simid, until=8*365, nostop=True, outfile=outname, with_movers=True, simdate=simdate, simtime=simtime, seed=i)
 
 if __name__ == "__main__":
