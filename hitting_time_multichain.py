3d237afa422af8219b2054ba2d28cea2dde5cbb3
dwu0042 <david.wu1@monash.edu>
2025-01-23 19:18:40 +1100

    add helper func to multichain

--- hitting_time_multichain.py
+++ hitting_time_multichain.py
@@ -1,6 +1,7 @@
 """
 This is an implementation of the computation of expected minimal hitting time over N identically parameterised continuous-time Markov chains {M_i(λ,Q)} 
 """
+
 import hitting_markov as mkv  # single chain computations
 import numpy as np
 from scipy import linalg
@@ -8,58 +9,70 @@ from scipy import integrate
 
 from typing import Iterable
 
+def without(Q, index=-1):
+    return np.delete(np.delete(Q, index, axis=0), index, axis=1)
+
 def construct_surival_ode(Q, index=-1):
     """Implement the problem u'(t) = Q_{A^cA^c} u(t)"""
     # remove row and column
-    Qcc = np.delete(np.delete(Q, index, axis=0), index, axis=1)
+    Qcc = without(Q, index) 
 
     def backward_equation(t, y):
-        return (Qcc @ y[:,np.newaxis]).flatten()
-    
+        return (Qcc @ y[:, np.newaxis]).flatten()
+
     u_0 = np.ones(Qcc.shape[0])
 
     return backward_equation, u_0
 
 
-"""
-STRATEGY
-
--> Set up the integration scheme using Gauss-Laguerre quadrature.
-    - requires the setup of Laguerre polynomials to solve for roots.
-        - can use np.polynomial.laguerre
--> Use scipy.integrate.solve_ivp to get u(t) at the given roots
--> Use w_i * (u(t) * Exp(t)) to solve the integral and return hitting times.
-"""
-
 def determine_laguerre_roots_and_weights(precision=30):
     return np.polynomial.laguerre.laggauss(precision)
 
-def solve_survival_ode(Nchains, Q, index=-1, precision=30):
+
+def solve_survival_ode(Q, index=-1, locations=None, endpoint=200):
+
+    ode_fun, ode_y0 = construct_surival_ode(Q, index=index)
+
+    if locations is not None:
+        tspan = [0, max(locations) * 1.05]
+    else:
+        tspan = [0, endpoint]
+
+    return integrate.solve_ivp(
+        fun=ode_fun,
+        t_span=tspan,
+        y0=ode_y0,
+        t_eval=locations,
+    )
+
+
+def compute_multichain_hitting_time(Nchains, Q, index=-1, precision=30):
     roots, weights = determine_laguerre_roots_and_weights(precision)
     gl_weights = weights * np.exp(roots)
 
-    survival_ode_fun, survival_ode_y0 = construct_surival_ode(Q, index=index)
+    survival_sol = solve_survival_ode(Q, index=index, locations=roots)
 
-    tspan = [0, max(roots)*1.1]
+    return np.dot(gl_weights, survival_sol.y.T**Nchains)
 
-    survival_sol = integrate.solve_ivp(
-        fun=survival_ode_fun,
-        t_span=tspan,
-        y0=survival_ode_y0,
-        t_eval=roots
-    )
-
-    return np.dot(gl_weights, survival_sol.y.T ** Nchains)
 
 def conform_compacted_array(hitting_arr, idx):
     N = np.shape(hitting_arr)[0]
-    full_arr = np.zeros(N+1)
+    full_arr = np.zeros(N + 1)
     full_arr[:idx] = hitting_arr[:idx]
-    full_arr[idx+1:] = hitting_arr[idx:]
+    full_arr[idx + 1 :] = hitting_arr[idx:]
     return full_arr
 
+
 def stitch_hitting_arrays(hitting_arrs: Iterable):
-    return np.vstack([
-        conform_compacted_array(arr, idx)
-        for idx, arr in enumerate(hitting_arrs)
-    ]).T
+    return np.vstack(
+        [conform_compacted_array(arr, idx) for idx, arr in enumerate(hitting_arrs)]
+    ).T
+
+def compute_all_multichain_hittings(Nchains, Q, precision=30):
+    return stitch_hitting_arrays(
+        [
+            compute_multichain_hitting_time(Nchains, Q, index=idx, precision=precision)
+            for idx in range(Q.shape[0])
+        ]
+    )
+
