e19a51f64999d0436458d78e688b60773a46630a
dwu0042 <david.wu1@monash.edu>
2025-05-28 08:51:15 +1000

    Update the home prop fig

--- scripts/run_zero_home_prop.py
+++ scripts/run_zero_home_prop.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 import functools
+from typing import Mapping
 import numpy as np
 from scipy import linalg, stats
 from matplotlib import pyplot as plt
+from matplotlib import dates as mpldates
 import seaborn as sns
 import polars as pl
 import h5py
@@ -35,6 +38,24 @@ def load():
 
     return histories
 
+_BASE_DATE = np.datetime64('2011-01', 'M')
+_MAX_DATE = np.datetime64('2018-12', 'M')
+def set_xdatelabels(hist, ax: plt.Axes, spacing=12, dt=1.0):
+    # here, we want to get succinct date formats
+    # we map the times onto the timedeltas of the simulations
+    N = hist.shape[1]
+    emp_max_date = (np.timedelta64(int(N*dt), 'D') + _BASE_DATE).astype('datetime64[M]')
+    
+    ax_max_date = np.min([emp_max_date, _MAX_DATE])
+
+    dates = np.arange(_BASE_DATE, ax_max_date, np.timedelta64(spacing, 'M'))
+
+    label_pos = (dates.astype('datetime64[D]').astype('int64') - _BASE_DATE.astype('datetime64[D]').astype('int64')) / dt
+
+    ax.set_xticks(label_pos, dates.astype('datetime64[Y]'), rotation=45)
+
+    return ax.get_xticks()
+
 def heatmap(hist, nmax=30):
     # hist is 2D
     bins = np.arange(nmax+2)
@@ -55,16 +76,17 @@ def plot_heatmap(*args, **kwargs):
 
 def mega_heatmap(hists, vmax=1500):
     hms = {model: heatmap(hist_vals)[1] for model, hist_vals in hists.items()}
-    fig, axs = plt.subplots(2, 2, figsize=[16, 9])
+    fig, axs = plt.subplots(nrows=4, figsize=[16, 9], sharex=True)
     for ax, (k, hmv) in zip(axs.flatten(), hms.items()):
-        sns.heatmap(data=hmv[::-1,:], vmax=vmax, ax=ax, cbar=False)
+        if k == "temporal": hmv=hmv[:,::2]
+        sns.heatmap(data=hmv[::-1,:], vmax=vmax, ax=ax, cmap='inferno', cbar=False)
         ax.set_title(k)
+        ax.set_ylim([-0.5, 30.5])
+    set_xdatelabels(hmv, ax)
+    fig.subplots_adjust(hspace=0.4)
     fig.colorbar(ax.collections[0], ax=axs, label='Count', extend='max')
-    fig.suptitle('Number at home')
 
     return fig
-
-
 
 if __name__ == "__main__":
     gather()
